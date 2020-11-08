ce074b523b297ba1f02f6fe6d6d07552dafa7779
kurdziel <marcin.kurdziel@nokia.com>
2020-11-08 17:19:15 +0100

    Improve GenericOperator

--- model/algorithms/genetic_operator.py
+++ model/algorithms/genetic_operator.py
@@ -4,17 +4,6 @@ from model.elements import Population
 
 
 class GeneticOperator(ABC):
-    __slots__ = "parameters"
-
-    def __init__(self):
-        self.parameters = {}
-
-    def __getitem__(self, item):
-        return self.parameters[item]
-
-    def __setitem__(self, key, value):
-        self.parameters[key] = value
-
     @abstractmethod
     def check_required_parameters(self):
         pass
@@ -22,3 +11,51 @@ class GeneticOperator(ABC):
     @abstractmethod
     def invoke(self, population: Population) -> Population:
         pass
+
+    def __setitem__(self, key, item):
+        self.__dict__[key] = item
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __repr__(self):
+        return repr(self.__dict__)
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __delitem__(self, key):
+        del self.__dict__[key]
+
+    def __cmp__(self, dict_):
+        return self.__cmp__(self.__dict__, dict_)
+
+    def __contains__(self, item):
+        return item in self.__dict__
+
+    def __iter__(self):
+        return iter(self.__dict__)
+
+    def clear(self):
+        return self.__dict__.clear()
+
+    def copy(self):
+        return self.__dict__.copy()
+
+    def has_key(self, k):
+        return k in self.__dict__
+
+    def update(self, *args, **kwargs):
+        return self.__dict__.update(*args, **kwargs)
+
+    def keys(self):
+        return self.__dict__.keys()
+
+    def values(self):
+        return self.__dict__.values()
+
+    def items(self):
+        return self.__dict__.items()
+
+    def pop(self, *args):
+        return self.__dict__.pop(*args)
