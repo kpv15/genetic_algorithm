d3fa1b8a6bfd582a0126954d9b28e78da0ae04b5
kurdziel <marcin.kurdziel@nokia.com>
2020-11-19 15:37:21 +0100

    Add fitness function for minimum and maximum

--- model/algorithms/fitness_functions.py
+++ model/algorithms/fitness_functions.py
@@ -1,4 +1,7 @@
+from math import exp, sqrt
+
 import numpy as np
+from typing import Dict
 
 from model.elements import Individual
 
@@ -8,3 +11,22 @@ def count_nonzero_fitness_function(individual: Individual):
     for chromosome in individual.chromosomes.values():
         fitness += np.count_nonzero(chromosome.genes)
     return fitness
+
+
+def _decoded_dict_to_numpy_array(decoded: Dict):
+    return np.asarray([pair[1] for pair in sorted(decoded.items(), key=lambda x: x[0])])
+
+
+def ackley_function(x: np.ndarray):
+    a, b, c, d = 20, 0.2, 2 * np.pi, x.shape[0]
+    return -a * exp(-b * sqrt((1/d) * np.sum(x**2))) - exp((1/d) * np.sum(np.cos(c*x))) + a + exp(1)
+
+
+# remember to use functools.partial in order to bind precision to this function
+def ackley_function_minimum_fitness_funtion(precision: Dict, individual: Individual):
+    return -ackley_function(_decoded_dict_to_numpy_array(individual.decode(precision)))
+
+
+# remember to use functools.partial in order to bind precision to this function
+def ackley_function_maximum_fitness_funtion(precision: Dict, individual: Individual):
+    return ackley_function(_decoded_dict_to_numpy_array(individual.decode(precision)))
