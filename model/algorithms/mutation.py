88579d6148b54a4befe920c9bda1affbeb741f0b
kurdziel <marcin.kurdziel@nokia.com>
2020-12-05 13:48:53 +0100

    Add UniformMutation

--- model/algorithms/mutation.py
+++ model/algorithms/mutation.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
-from random import uniform, sample
+from random import uniform, sample, randrange
 
 from model.algorithms.genetic_operator import GeneticOperator
-from model.elements import Population, Chromosome
+from model.elements import Population, Chromosome, make_real_chromosome
 
 
 class Mutation(GeneticOperator, ABC):
@@ -45,3 +45,19 @@ class Inversion(Mutation):
         indexes = sample(range(0, len(chromosome) + 1), 2)
         chromosome.partial_logical_not_inplace(list(range(indexes[0], indexes[1]) if indexes[0] < indexes[1]
                                                     else range(indexes[1], indexes[0])))
+
+
+class UniformMutation(Mutation):
+    def check_required_parameters(self):
+        if "min" not in self:
+            raise KeyError("UniformMutation::'min' is missing")
+        if "max" not in self:
+            raise KeyError("UniformMutation::'max' is missing")
+        if "precision" not in self:
+            raise KeyError("UniformMutation::'precision' is missing")
+        if "fill" not in self:
+            raise KeyError("UniformMutation::'fill' is missing")
+        super().check_required_parameters()
+
+    def _mutate(self, chromosome: Chromosome):
+        chromosome.replace(make_real_chromosome(randrange(self["min"], self["max"]), self["precision"], self["fill"]))
